a17d60632f3ec94e1725cb7d0b4e95ac661bcbd0
biphobe <hi@bipho.be>
2017-11-22 01:11:05 +0100

    Add CSV generation function

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const fs = require("fs");
+
 const fetch = require("node-fetch");
 
 require('dotenv').config();
 
 const todoistToken = process.env.TODOIST_TOKEN;
+const csvTemplate = fs.readFileSync("template.csv", "utf8");
 
 function getTodoistData(token) {
     return fetch("https://todoist.com/api/v7/sync", {
@@ -13,6 +16,17 @@ function getTodoistData(token) {
         method: "POST"
     })
         .then(res => res.json());
+}
+
+function generateCsv(content, template) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth()+1;
+    const day = date.getDate();
+
+    return template
+        .replace("{{date}}", `${year}-${month}-${day}`)
+        .replace("{{content}}", content);
 }
 
 getTodoistData(todoistToken)
