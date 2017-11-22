6f1d9ec2b40449a3fb236dfaae10b6cee109aff7
biphobe <hi@bipho.be>
2017-11-22 01:04:19 +0100

    Create getTodoistData function

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const fetch = require("node-fetch");
 
 require('dotenv').config();
 
-fetch("https://todoist.com/api/v7/sync", {
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: `token=${process.env.TODOIST_TOKEN}&sync_token=*&resource_types=[\"all\"]`,
-    method: "POST"
-})
-    .then(res => res.json())
+const todoistToken = process.env.TODOIST_TOKEN;
+
+function getTodoistData(token) {
+    return fetch("https://todoist.com/api/v7/sync", {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `token=${token}&sync_token=*&resource_types=[\"all\"]`,
+        method: "POST"
+    })
+        .then(res => res.json());
+}
+
+getTodoistData(todoistToken)
     .then(console.log)
     .catch(console.error);
